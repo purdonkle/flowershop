665b5f808d981285de3300c7954abc8412935723
purdonkle <adampurdondev@gmail.com>
2020-02-23 22:23:48 +0200

    edited unit test to create mock of Flower

--- FlowerShopTests/UnitTests.cs
+++ FlowerShopTests/UnitTests.cs
@@ -30,15 +30,15 @@ namespace Tests
             /*Arrange*/
             var orderDAO = Substitute.For<IOrderDAO>();
 			var client = Substitute.For<IClient>();
-            Order order = new Order(orderDAO, client);
+            var order = Substitute.For<Order>(orderDAO, client);
             var flowerDAO = Substitute.For<IFlowerDAO>();
-            var flower = Substitute.For<IFlower>();
+            var flower = Substitute.For<Flower>(flowerDAO, "lily", 10, 5);
 
             /*Act*/
             order.AddFlowers(flower, 2);
 
             /*Assert*/
-            Assert.AreEqual(order.Price, 12.00);
+            Assert.AreEqual(order.Price, 24.00);
         }
     }
 }
